1e1053641a6a21cede1ec6a46197a2063629dc25
VelinovAngel <kakarum1992@gmail.com>
2021-01-16 00:02:43 +0100

    Create database SoftUni

--- Exercises Database Introduction/SoftUni Database.sql
+++ Exercises Database Introduction/SoftUni Database.sql
@@ -14,8 +14,6 @@ insert into Towns([Name]) values
 
 select * from Towns
 
-
-
 --•	Addresses (Id, AddressText, TownId)
 create table Addresses
 (
@@ -50,6 +48,22 @@ create table Employees
 	Salary decimal(18,2),
 	AddressId int foreign key references Addresses(Id)
 )
+insert into Employees(FirstName,MiddleName,LastName,JobTitle,DepartmentId,HireDate,Salary)
+	values 
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '02/01/2013', 3500.00),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '03/02/2004', 4000.00),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '08/28/2016', 525.25),
+('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '12/09/2007', 3000.00),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '08/28/2016', 599.88)
+
+select * from Employees
+--Name					Job Title		Department				Hire Date	Salary
+--Ivan Ivanov Ivanov	.NET Developer	Software Development	01/02/2013	3500.00
+--Petar Petrov Petrov	Senior Engineer	Engineering				02/03/2004	4000.00
+--Maria Petrova Ivanova	Intern			Quality Assurance		28/08/2016	525.25
+--Georgi Teziev Ivanov	CEO				Sales					09/12/2007	3000.00
+--Peter Pan Pan			Intern			Marketing				28/08/2016	599.88
+
 
 --drop table Employees
 
