48b2e23af11227f159aff41f868727247bd608fa
VelinovAngel <kakarum1992@gmail.com>
2021-01-16 18:36:15 +0100

    Modify queries from previous problem to show only some of the columns

--- Exercises Database Introduction/SoftUni Database.sql
+++ Exercises Database Introduction/SoftUni Database.sql
@@ -43,20 +43,22 @@ create table Employees
 	MiddleName varchar(90),
 	LastName varchar(90),
 	JobTitle varchar(90),
-	DepartmentId int foreign key references Departments(Id),
+	DepartmentId int foreign key references Departments(id),
 	HireDate datetime,
 	Salary decimal(18,2),
 	AddressId int foreign key references Addresses(Id)
 )
 insert into Employees(FirstName,MiddleName,LastName,JobTitle,DepartmentId,HireDate,Salary)
 	values 
-('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '02/01/2013', 3500.00),
-('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '03/02/2004', 4000.00),
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '01/02/2013', 3500.00),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '02/03/2004', 4000.00),
 ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '08/28/2016', 525.25),
-('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '12/09/2007', 3000.00),
-('Peter', 'Pan', 'Pan', 'Intern', 3, '08/28/2016', 599.88)
+('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '09/12/2007', 3000.00),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '12/08/2016', 599.88)
 
 select * from Employees
+
+drop table Employees
 --Name					Job Title		Department				Hire Date	Salary
 --Ivan Ivanov Ivanov	.NET Developer	Software Development	01/02/2013	3500.00
 --Petar Petrov Petrov	Senior Engineer	Engineering				02/03/2004	4000.00
