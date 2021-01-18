33691ac373a808fb28f0926063cd333a1caa23f9
VelinovAngel <kakarum1992@gmail.com>
2021-01-18 18:19:57 +0100

    Exercises on CRUD funcitions

---
+++ Crud Exercises/Crud Exercises from SoftUni database.sql
@@ -0,0 +1,15 @@
+use SoftUni
+
+
+	--02. Find All Information About Departments
+/*-------------------------------------------------*/
+
+select * from Departments
+
+	--3.Find all Department Names
+/*-------------------------------------------------*/
+
+select [Name] from Departments
+
+	--4.Find Salary of Each Employee
+/*-------------------------------------------------*/
