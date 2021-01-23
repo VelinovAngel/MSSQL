0541c1263d5f7d1ab4ce0bcf388a34c2aa675b10
VelinovAngel <kakarum1992@gmail.com>
2021-01-23 22:49:17 +0100

    Add problem 11

--- Exercises Built-in Functions/Exercises Built-in Functions.sql
+++ Exercises Built-in Functions/Exercises Built-in Functions.sql
@@ -82,4 +82,19 @@ SELECT [EmployeeID], [FirstName], [LastName], [Salary],
 	 WHERE Salary BETWEEN 10000 AND 50000
 	 ORDER BY Salary DESC
 
+	 --Problem 11.Find All Employees with Rank 2 *
+/*----------------------------------------------------------*/
+
+--Use the query from the previous problem and upgrade it, so that it finds only the employees whose Rank is 2 and again, order them by Salary (descending).
+
+SELECT * 
+	FROM(SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+			 DENSE_RANK() OVER (PARTITION BY Salary 
+						  ORDER BY EmployeeID) AS [Rank]
+		FROM Employees
+			 WHERE Salary BETWEEN 10000 AND 50000) AS [Rank]
+			 WHERE [Rank] = 2
+			 ORDER BY Salary DESC
+
+
 	 
