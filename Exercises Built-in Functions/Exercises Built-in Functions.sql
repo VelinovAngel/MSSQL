5a5b1d3b3fb87f0f021fb04328600a77635e0b3b
VelinovAngel <kakarum1992@gmail.com>
2021-01-23 21:44:27 +0100

    Exercises on Build-in Functions

---
+++ Exercises Built-in Functions/Exercises Built-in Functions.sql
@@ -0,0 +1,69 @@
+--USE SoftUni
+
+	--Problem 1.Find Names of All Employees by First Name
+/*----------------------------------------------------------*/
+
+SELECT FirstName, LastName FROM Employees
+	WHERE FirstName LIKE 'SA%'
+
+	--Problem 2.Find Names of All employees by Last Name 
+/*----------------------------------------------------------*/
+
+SELECT [FirstName], [LastName] FROM Employees
+	WHERE [LastName] LIKE '%ei%'
+
+
+	--Problem 3.Find First Names of All Employees 
+/*----------------------------------------------------------*/
+
+--Write a SQL query to find the first names of all employees in the departments 
+--with ID 3 or 10 and whose hire year is between 1995 and 2005 inclusive.
+
+SELECT [FirstName] FROM Employees
+	WHERE DepartmentID in (3,10)
+		AND YEAR(HireDate) BETWEEN 1995 AND 2005
+	
+	
+	--Problem 4.Find All Employees Except Engineers
+/*----------------------------------------------------------*/
+
+SELECT [FirstName], [LastName] FROM Employees
+	WHERE JobTitle NOT LIKE '%engineer%'
+
+
+	--Problem 5.Find Towns with Name Length
+/*----------------------------------------------------------*/
+
+--Write a SQL query to find town names that are 5 or 6 symbols long and order them alphabetically by town name.
+
+SELECT [Name] FROM Towns
+	WHERE LEN(Name) in (5,6)
+	ORDER BY [Name] ASC
+
+
+	--Problem 6.Find Towns Starting With
+/*----------------------------------------------------------*/
+
+--Write a SQL query to find all towns that start with letters M, K, B or E. Order them alphabetically by town name. 
+
+SELECT [TownID], [Name] FROM Towns
+	WHERE [Name] LIKE '[MBKE]%'
+	ORDER BY [Name] ASC
+
+	--Problem 7.Find Towns Not Starting With
+/*----------------------------------------------------------*/
+
+SELECT [TownID], [Name] From Towns
+	WHERE [Name] NOT LIKE '[RBD]%'
+	ORDER BY [Name] ASC
+
+	--Problem 8.Create View Employees Hired After 2000 Year
+/*----------------------------------------------------------*/
+
+CREATE VIEW V_EmployeesHiredAfter2000 AS
+	SELECT [FirstName], [LastName] FROM Employees
+		WHERE YEAR(HireDate) > 2000
+
+		--Problem 9.Length of Last Name
+/*----------------------------------------------------------*/
+
