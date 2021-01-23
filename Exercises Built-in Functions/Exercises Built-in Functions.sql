4327745a5d9612da4f5254b8b27ea3d8e81d8be5
VelinovAngel <kakarum1992@gmail.com>
2021-01-23 23:03:45 +0100

    Add problem 12

--- Exercises Built-in Functions/Exercises Built-in Functions.sql
+++ Exercises Built-in Functions/Exercises Built-in Functions.sql
@@ -96,5 +96,13 @@ SELECT *
 			 WHERE [Rank] = 2
 			 ORDER BY Salary DESC
 
+	 --Problem 12.	Countries Holding ‘A’ 3 or More Times
+/*----------------------------------------------------------*/
+
+--USE Geography
+
+SELECT [CountryName], [IsoCode] FROM Countries
+	WHERE CountryName LIKE '%a%a%a%'
+		ORDER BY IsoCode
 
 	 
