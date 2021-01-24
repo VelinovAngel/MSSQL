f65b30a805629feac5eff54eb6b98db15986bdc1
VelinovAngel <kakarum1992@gmail.com>
2021-01-24 21:33:19 +0100

    Add problem 17

--- Exercises Built-in Functions/Exercises Built-in Functions.sql
+++ Exercises Built-in Functions/Exercises Built-in Functions.sql
@@ -142,3 +142,29 @@ SELECT [Username], SUBSTRING ( [Email], CHARINDEX( '@', [Email] ) + 1,
 SELECT [Username], [IpAddress] AS [IP Address] FROM Users
 	WHERE [IpAddress] LIKE '___.1%.%.___'
 	ORDER BY [Username]
+
+	--Problem 17.Show All Games with Duration and Part of the Day
+/*----------------------------------------------------------*/	
+
+--Find all games with part of the day and duration sorted by game name alphabetically then by duration (alphabetically, not by the timespan) and part of the day (all ascending). Parts of the day should be Morning (time is >= 0 and < 12), Afternoon (time is >= 12 and < 18), Evening (time is >= 18 and < 24). Duration should be Extra Short (smaller or equal to 3), Short (between 4 and 6 including), Long (greater than 6) and Extra Long (without duration).
+
+SELECT [Name] AS [Game],
+		CASE
+		WHEN DATEPART(HOUR, [Start]) BETWEEN 0 AND 11 THEN 'Morning'
+		WHEN DATEPART(HOUR, [Start]) BETWEEN 12 AND 17 THEN 'Afternoon'
+		WHEN DATEPART(HOUR, [Start]) BETWEEN 18 AND 24 THEN 'Evening'
+		END
+		AS [Part of the Day],
+		CASE
+		WHEN Duration <= 3 THEN 'Extra Short'
+		WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+		WHEN Duration > 6 THEN 'Long'
+		ELSE 
+		'Extra Long'
+		END
+		AS [Duration]		
+FROM Games
+	ORDER BY [Name], [Duration], [Part of the Day] ASC
+
+
+/*----------------------------------------------------------*/	
