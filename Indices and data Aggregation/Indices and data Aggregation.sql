38b34c3b623f1f6d23f723e719d2f1c3e6d56d6a
VelinovAngel <kakarum1992@gmail.com>
2021-01-30 18:17:33 +0100

    Exercises on Indices and Data aggregation

---
+++ Indices and data Aggregation/Indices and data Aggregation.sql
@@ -0,0 +1,9 @@
+
+
+	--1. Records’ Count
+/*-----------------------------------------------------*/
+--Import the database and send the total count of records from the one and only table to Mr. Bodrog. Make sure nothing got lost.
+
+SELECT COUNT(FirstName) AS Count
+	FROM WizzardDeposits
+
