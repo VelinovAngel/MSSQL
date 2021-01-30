19ffe57f20aa5a48348344cc3e2f691221756b5d
VelinovAngel <kakarum1992@gmail.com>
2021-01-30 18:46:49 +0100

    Add problem 4

--- Indices and data Aggregation/Indices and data Aggregation.sql
+++ Indices and data Aggregation/Indices and data Aggregation.sql
@@ -14,9 +14,30 @@ SELECT COUNT(FirstName) AS Count
 SELECT MAX(MagicWandSize) AS LongestMagicWand
 	FROM WizzardDeposits
 
+
 	--3. Longest Magic Wand Per Deposit Groups
 /*-----------------------------------------------------*/
+--For wizards in each deposit group show the longest magic wand. Rename the new column appropriately.
 
 SELECT DepositGroup, MAX(MagicWandSize) AS LongestMagicWand
 	FROM WizzardDeposits
 	GROUP BY DepositGroup
+
+
+	--4. * Smallest Deposit Group Per Magic Wand Size
+/*-----------------------------------------------------*/
+--Select the two deposit groups with the lowest average wand size.
+
+SELECT TOP(2) DepositGroup
+	FROM (
+			SELECT DepositGroup, AVG(MagicWandSize) AS MWS
+				FROM WizzardDeposits
+				GROUP BY DepositGroup
+		 ) AS A
+		 ORDER BY MWS
+
+	
+--SELECT TOP(2) DepositGroup 
+--	FROM WizzardDeposits
+--	GROUP BY DepositGroup
+--	ORDER BY AVG(MagicWandSize)
