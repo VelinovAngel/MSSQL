95cca911e20ae32e74ff1a6039715ba2772fb58c
VelinovAngel <kakarum1992@gmail.com>
2021-01-30 19:23:50 +0100

    Add problem 7

--- Indices and data Aggregation/Indices and data Aggregation.sql
+++ Indices and data Aggregation/Indices and data Aggregation.sql
@@ -65,3 +65,10 @@ SELECT DepositGroup, SUM(DepositAmount)
 /*-----------------------------------------------------*/
 --Select all deposit groups and their total deposit sums but only for the wizards who have their magic wands crafted by Ollivander family. Filter total deposit amounts lower than 150000. Order by total deposit amount in descending order.
 	
+	
+SELECT DepositGroup, SUM(DepositAmount) AS D
+	FROM WizzardDeposits
+	WHERE MagicWandCreator = 'Ollivander family'
+	GROUP BY DepositGroup
+	HAVING SUM(DepositAmount) < 150000
+	ORDER BY D DESC
