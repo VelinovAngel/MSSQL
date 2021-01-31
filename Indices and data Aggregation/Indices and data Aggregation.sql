932d02e277e87a44b3c6e4ceb4ad125f8bd530cb
VelinovAngel <kakarum1992@gmail.com>
2021-01-31 12:00:11 +0100

    Add problem 11

--- Indices and data Aggregation/Indices and data Aggregation.sql
+++ Indices and data Aggregation/Indices and data Aggregation.sql
@@ -138,3 +138,14 @@ SELECT FN
 	GROUP BY FN
 	ORDER BY FN
 
+	
+	--11. Average Interest 
+/*-----------------------------------------------------*/
+--Mr. Bodrog is highly interested in profitability. He wants to know the average interest of all deposit groups split by whether the deposit has expired or not. But that’s not all. He wants you to select deposits with start date after 01/01/1985. Order the data descending by Deposit Group and ascending by Expiration Flag.
+
+SELECT DepositGroup, IsDepositExpired, AVG(DepositInterest)
+	FROM WizzardDeposits
+	WHERE DepositStartDate > '1985-01-01'
+	GROUP BY DepositGroup, IsDepositExpired
+	ORDER BY DepositGroup DESC , IsDepositExpired ASC
+
