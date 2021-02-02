67563f1d024305141e1fa8aaa0e29a720b845a13
VelinovAngel <kakarum1992@gmail.com>
2021-02-02 17:42:46 +0100

    Functions and Stored Procedures Exercises

---
+++ FUNCTIONS AND STORED PROCEDURES/Functions and Stored Procedures.sql
@@ -0,0 +1,32 @@
+
+--1.Employees with Salary Above 35000
+/*----------------------------------------*/
+--Create stored procedure usp_GetEmployeesSalaryAbove35000 that returns all employees’ first and last names for whose salary is above 35000. 
+
+
+
+
+--2.Employees with Salary Above Number
+/*----------------------------------------*/
+--Create stored procedure usp_GetEmployeesSalaryAboveNumber that accept a number (of type DECIMAL(18,4)) as parameter and returns all employees’ first and last names whose salary is above or equal to the given number. 
+
+
+
+
+--3.Town Names Starting With
+/*----------------------------------------*/
+
+
+
+--4.Employees from Town
+/*----------------------------------------*/
+
+
+
+
+
+--5.Salary Level Function
+/*----------------------------------------*/
+
+
+
