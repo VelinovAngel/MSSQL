a762d8e68c633f3d401c4a76e92f30ba8c869aef
VelinovAngel <kakarum1992@gmail.com>
2021-02-10 19:49:28 +0100

    Create all tables

--- Exam Preparation/Databases MSSQL Server Exam - 27 Jun 2020/Database Basics MS SQL Exam – 27 June 2020.sql
+++ Exam Preparation/Databases MSSQL Server Exam - 27 Jun 2020/Database Basics MS SQL Exam – 27 June 2020.sql
@@ -29,7 +29,7 @@ CREATE TABLE Models(
 CREATE TABLE Jobs(
 	JobId INT NOT NULL PRIMARY KEY IDENTITY,
 	ModelId INT NOT NULL REFERENCES Models(ModelId),
-	[Status] NVARCHAR(11) CHECK([Status] IN ('Pending', 'Finished')) DEFAULT 'Pending' NOT NUll,
+	[Status] NVARCHAR(11) CHECK([Status] IN ('Pending', 'In Progress', 'Finished')) DEFAULT 'Pending' NOT NUll,
 	ClientId INT NOT NULL REFERENCES Clients(ClientId),
 	MechanicId INT REFERENCES Mechanics(MechanicId),
 	IssueDate DATE,
@@ -54,7 +54,7 @@ CREATE TABLE Parts(
 	[Description] NVARCHAR(255),
 	Price DECIMAL (6,2) CHECK(Price > 0),
 	VendorId INT NOT NULL REFERENCES Vendors(VendorId),
-	StockQty INT CHECK(StockQty < 0) DEFAULT 0
+	StockQty INT CHECK(StockQty >= 0) DEFAULT 0
 )
 
 CREATE TABLE OrderParts(
@@ -75,3 +75,20 @@ CREATE TABLE PartsNeeded(
 				--Section 2. DML
 --2.Insert
 /*------------------------------------------------*/
+
+INSERT INTO Clients(FirstName, LastName , Phone) VALUES
+('Teri',   'Ennaco', '570-889-5187'),
+( 'Merlyn', 'Lawler', '201-588-7810'),
+( 'Georgen','Montezuma', '925-615-5185'),
+( 'Jettie', 'Mconnell', '908-802-3564'),
+( 'Lemuel', 'Latzke', '631-748-6479'),
+( 'Melodie','Knipp', '805-690-1682'),
+ ('Candida','Corbley', '908-275-8357')
+
+
+INSERT INTO Parts(SerialNumber, [Description], Price, VendorId)
+	VALUES
+	('WP8182119', 'Door Boot Seal', 117.86, 2),
+	('W10780048', 'Suspension Rod', 42.81, 1),
+	('W10841140', 'Silicone Adhesive ', 6.77, 4),
+	('WPY055980', 'High Temperature Adhesive', 13.94, 3)
